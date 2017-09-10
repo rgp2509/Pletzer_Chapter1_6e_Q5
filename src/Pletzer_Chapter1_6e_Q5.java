78529a1acfe267aec61a7d6b96577996669cc6f8
RyanP <rgp2509@email.vccs.edu>
2017-09-10 19:42:19 -0400

    Pletzer, Ryan; Chapter 1, 6th edition, Chapter 5 All rights reserved.

---
+++ src/Pletzer_Chapter1_6e_Q5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Pletzer_Chapter1_6e_Q5 {
+    public static void main(String[] args) {
+        /* Code Created by Ryan Pletzer, All rights reserved.
+        This code was generated to show a solution to the problem found in "Java: an introduction to computer
+        programing and solving" By Walter Savitch
+
+        This code used a simple for loop with a +1 incrementer to make an otherwise tedious problem simple.
+        While using proper print syntax to ensure a nice final print.
+        This code was also only written in the main, as it was short enough to complete in only a few lines.
+
+         */
+        System.out.println("Welcome to the integer display tool, I will output all integers between two values");
+        System.out.println("Example Inputs -1, - 5; -1, 0, 1, 2, 3, 4, and 5.");
+        System.out.println("Please enter an initial integer now");
+        Scanner input = new Scanner(System.in);
+        int first = input.nextInt();
+        System.out.println("Please enter a final integer now");
+        int last = input.nextInt();
+        System.out.print("your integers are: ");
+        for (int i = first; i < last; i++){
+            System.out.print(i +", " );
+        }
+        System.out.print("and " + last + "." );
+    }
+}
---
+++ src/Scanner.java
@@ -0,0 +1,6 @@
+import java.io.InputStream;
+
+public class Scanner {
+    public Scanner(InputStream in) {
+    }
+}
